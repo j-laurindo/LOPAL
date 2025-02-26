326b19df6ba6303a79e54aee89ed663312ec1618
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-02-26 15:26:44 -0300

    Add files via upload

---
+++ PycharmProjects/PythonProject/OpAritm.py
@@ -0,0 +1,69 @@
+"""
+# Exercício 1
+
+x = float(input("Insira o 1º número: "))
+y = float(input("Insira o 2º número: "))
+z = float(input("Insira o 3º número: "))
+
+media = (x + y + z) / 3
+
+print(f"A média aritética dos 3 números é igual à: {media}" )
+"""
+
+"""
+# Exercício 2
+
+valor = float(input("Escreva o valor original da prestação: "))
+
+taxa = (valor * 10) / 100
+total = valor + taxa
+
+print(f"O valor + juros é igual à: {total}")
+"""
+
+"""
+# Exercício 3
+
+n1 = int(input("Digite um 1º Número: "))
+n2 = int(input("Digite um 2º Número: "))
+n3 = int(input("Digite um 3º Número: "))
+
+media = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / ( 2 + 3 + 5 )
+
+print(f"A média ponderada é: {media}")
+"""
+
+"""
+#Exercício 4 - Operador de Comparação
+
+a = 5
+b = 10
+
+print(a == b)
+print(a != b)
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+"""
+
+"""
+# Exercício 5 - Operador de Comparação
+
+num = int(input("Digite um número: "))
+
+print(f"Positivo - True  |  Negativo - False : ", num > 0)
+"""
+
+"""
+# Exercício 6 - Operador de Comparação
+
+num = int(input("Digite um número: "))
+resultado = (num % 5 == 0)
+
+print(f"Ele é múltiplo de 5 - True  |  Ele não é múltiplo de 5 - False:", resultado)
+"""
+
+
+
+
---
+++ PycharmProjects/PythonProject/main.py
@@ -0,0 +1,127 @@
+mensagem = "oi, python"
+numero = 5
+pi = 4.14
+
+print(mensagem)
+print(numero)
+print(pi)
+
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+# Exercicio 1
+
+nome = "Julia Roberts"
+idade = 18
+
+print(f"Boa tarde 1DS-TB17! Meu nome é {nome} e tenho {idade} anos.")
+
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+
+# Exercicio 2
+
+""" 
+nome1 = "Aula"
+nome2 = "Python"
+
+print(nome1, nome2)
+
+OutroNome1 = nome2
+
+print(OutroNome1, nome1)
+"""
+
+
+nome1 = "Aula"
+nome2 = "Python"
+
+print(nome1, nome2)
+
+nome1, nome2 = nome2, nome1
+
+print(nome1,nome2)
+
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+
+# Exercicio 3
+
+nome = "Julia"
+sobrenome = "Laurindo"
+
+juncao = nome + " " + sobrenome
+
+print(juncao)
+
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+
+# Exercicio 4
+
+NumInt = 3
+NumDec = float(NumInt)
+
+print(f"Resultado do número inteiro convertido para decimal: {NumDec}")
+
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+# Exercício 5
+
+alimento1 = "Arroz"
+alimento2 = "Feijão"
+alimento3 = "Carne Acebolada"
+frase = alimento1 + ", " + alimento2 + ", e " + alimento3
+
+print(f"Hoje vou fazer o almoço. Vai ser {frase}")
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+# Exercicio 6
+
+
+dia = 1
+mes = 12
+ano = 2025
+data = f"{dia:02d}/{mes:02d}/{ano}"
+
+print(data)
+
+print(f" ")
+print(f"----------------------------------------------")
+print(f" ")
+
+# Exercicio 7
+
+frase = "Gosto de comer cachorro quente de rua"
+
+print(frase)
+print(frase[:23])
+
+
+
+
+
+
+
+
+
+
+
+
